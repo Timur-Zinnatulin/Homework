445b31bf20e41917a6a884b1a62539ed525172b2
Timur Zinnatulin <teemychteemych@gmail.com>
2018-11-27 20:12:50 +0300

    Added file input/output functionality

---
+++ Homework-4/hw-4.3/file.cpp
@@ -0,0 +1,30 @@
+#include "file.h"
+#include "phonebook.h"
+
+//Reads records from an existing file or creates a new one with given name if it doesn't exist
+void openFile(Record *phonebook, int &phonebookSize, const char *fileName)
+{
+	FILE *file = fopen(fileName, "a+");
+	phonebookSize = 0;
+	while (!feof(file))
+	{
+		const int readBytes = fscanf(file, "%s%d", &phonebook[phonebookSize].name, &phonebook[phonebookSize].number);
+		if (readBytes < 0)
+		{
+			break;
+		}
+		++phonebookSize;
+	}
+	fclose(file);
+}
+
+//Puts the database of records into a file
+void saveRecords(Record *phonebook, int phonebookSize, const char *fileName)
+{
+	FILE *file = fopen(fileName, "w");
+	for (int i = 0; i < phonebookSize; ++i)
+	{
+		fprintf(file, "%s %d\n", phonebook[i].name, phonebook[i].number);
+	}
+	fclose(file);
+}
---
+++ Homework-4/hw-4.3/file.h
@@ -0,0 +1,7 @@
+#pragma once
+
+//Reads records from an existing file or creates a new one with given name if it doesn't exist
+void openFile(Record *phonebook, int &phonebookSize, const char *fileName);
+
+//Puts the database of records into a file
+void saveRecords(Record *phonebook, int phonebookSize, const char *fileName);

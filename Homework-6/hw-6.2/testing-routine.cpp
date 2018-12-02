1ecc1b6543a93f562236b71872fd3de1e4e515e5
Timur Zinnatulin <teemychteemych@gmail.com>
2018-12-02 20:31:05 +0300

    Added pre-run tests

---
+++ Homework-6/hw-6.2/testing-routine.cpp
@@ -0,0 +1,25 @@
+#include "stack.h"
+#include "bracket-control.h"
+#include "testing-routine.h"
+
+//Pre-run testing function
+bool testingRoutine()
+{
+	char test1[15] = "([])([{}])({})";
+	char test2[3] = "(}";
+	char test3[5] = "()}{";
+	const bool answer1 = true;
+	const bool answer2 = false;
+	const bool answer3 = false;
+	if (correctSequence(test1) == answer1)
+	{
+		if (correctSequence(test2) == answer2)
+		{
+			if (correctSequence(test3) == answer3)
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}

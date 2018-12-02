f1cc5f5f50c39c571f3feeb3939a1b65c2c22053
Timur Zinnatulin <teemychteemych@gmail.com>
2018-12-02 18:08:19 +0300

    Completed the main part of the program. Task complete.

--- Homework-6/hw-6.1/hw-6.1.cpp
+++ Homework-6/hw-6.1/hw-6.1.cpp
@@ -1,10 +1,37 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include "stack.h"
 #include "testing-routine.h"
 #include "expression-calculator.h"
 
 int main()
 {
+	if (testingRoutine)
+	{
+		printf("Testing successful! Ready to run.\n\n");
+	}
+	else
+	{
+		printf("Testing unsuccessful :(\n");
+		return 0;
+	}
+	printf("Welcome to Reverse Polish Notation Calculator ver.1!\n");
+	printf("Please enter a string of digits and signs in this notation that you wish to calculate.\n");
+	printf("Input: ");
+	char *input = new char[100]{};
+	scanf("%[^\n]s", input);
+	auto stack = createNewStack();
+	bool correctionCheck = calculateInput(stack, input);
+	if (!correctionCheck)
+	{
+		printf("There was division by zero!!\n");
+		printf("Cannot calculate, exiting...");
+	}
+	else
+	{
+		printf("The result of calculation is %d\n", pop(stack));
+	}
+	deleteStack(stack);
 	return 0;
 }
 

b63d081ed1219fc897a2793525a0dc1905bfaead
Timur Zinnatulin <teemychteemych@gmail.com>
2018-12-02 19:29:22 +0300

    Initial commit for problem 6.2

---
+++ Homework-6/hw-6.2/stack.cpp
@@ -0,0 +1,55 @@
+#include "stack.h"
+
+struct StackElement
+{
+	char value = ' ';
+	StackElement *next = nullptr;
+};
+
+struct Stack
+{
+	StackElement *head = nullptr;
+};
+
+//Checks if the stack is empty
+bool isEmpty(Stack *stack)
+{
+	return (stack->head == nullptr);
+}
+
+//Creates a new empty stack
+Stack *createNewStack()
+{
+	return new Stack;
+}
+
+//Deletes the stack
+void deleteStack(Stack *stack)
+{
+	while (!isEmpty(stack))
+	{
+		const auto tempStackElement = stack->head;
+		stack->head = stack->head->next;
+		delete tempStackElement;
+	}
+	delete stack;
+}
+
+//Pushes a value into the stack
+void push(Stack *stack, char newValue)
+{
+	const auto newElement = new StackElement{ newValue, stack->head };
+	stack->head = newElement;
+}
+
+//Pops a value out of the stack
+void pop(Stack *stack)
+{
+	if (isEmpty(stack))
+	{
+		return;
+	}
+	StackElement *poppedHead = stack->head;
+	stack->head = stack->head->next;
+	delete poppedHead;
+}
---
+++ Homework-6/hw-6.2/stack.h
@@ -0,0 +1,20 @@
+#pragma once
+
+struct StackElement;
+
+struct Stack;
+
+//Checks if the stack is empty
+bool isEmpty(Stack *stack);
+
+//Creates a new empty stack
+Stack *createNewStack();
+
+//Deletes the stack
+void deleteStack(Stack *stack);
+
+//Pushes a value into the stack
+void push(Stack *stack, char newValue);
+
+//Pops a value out of the stack
+void pop(Stack *stack);

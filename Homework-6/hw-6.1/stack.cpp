54f9fe842002841284415f44bc88c6dee110079c
Timur Zinnatulin <teemychteemych@gmail.com>
2018-12-02 17:15:51 +0300

    Finished writing the stack functionality

--- Homework-6/hw-6.1/stack.cpp
+++ Homework-6/hw-6.1/stack.cpp
@@ -1,1 +1,52 @@
 #include "stack.h"
+
+struct StackElement
+{
+	int value = 0;
+	StackElement *next = nullptr;
+};
+
+struct Stack
+{
+	StackElement *head = nullptr;
+};
+
+bool isEmpty(Stack *stack)
+{
+	return (stack->head == nullptr);
+}
+
+Stack *createNewStack()
+{
+	return new Stack;
+}
+
+void deleteStack(Stack *stack)
+{
+	while (!isEmpty(stack))
+	{
+		const auto tempStackElement = stack->head;
+		stack->head = stack->head->next;
+		delete tempStackElement;
+	}
+	delete stack;
+}
+
+void push(Stack *stack, int newValue)
+{
+	const auto newElement = new StackElement{ newValue, stack->head };
+	stack->head = newElement;
+}
+
+int pop(Stack *stack)
+{
+	if (isEmpty(stack))
+	{
+		return 0;
+	}
+	StackElement *poppedHead = stack->head;
+	stack->head = stack->head->next;
+	int value = poppedHead->value;
+	delete poppedHead;
+	return value;
+}

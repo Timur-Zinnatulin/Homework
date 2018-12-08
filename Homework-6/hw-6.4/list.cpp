725bb9d9ad2d4d76b2ebba2338ffb171040db557
Timur Zinnatulin <teemychteemych@gmail.com>
2018-12-09 02:28:52 +0300

    Added list functionality, with merge and split functions

--- Homework-6/hw-6.4/list.cpp
+++ Homework-6/hw-6.4/list.cpp
@@ -1,1 +1,102 @@
 #include "list.h"
+#include <string>
+
+using namespace std;
+
+struct Node
+{
+	string name{};
+	string number{};
+	Node *previous = nullptr;
+	Node *next;
+};
+
+struct List
+{
+	Node *start = nullptr;
+	Node *end = nullptr;
+	int size = 0;
+};
+
+//Creates a new list
+List *createNewList()
+{
+	return new List;
+}
+
+//Checks if the list is empty
+bool isEmpty(List *list)
+{
+	return (list->start == nullptr);
+}
+
+//Deletes the list
+void deleteList(List *list)
+{
+	while (list->start != list->end)
+	{
+		const auto temp = list->start;
+		list->start = list->start->next;
+		delete temp;
+	}
+	delete list->start;
+	delete list;
+}
+
+//Creates a nww node at the end of the list
+void push(List *list, const string name, const string number)
+{
+	list->end = new Node{ name, number, list->end, nullptr };
+	if (isEmpty(list))
+	{
+		list->start = list->end;
+	}
+	else
+	{
+		list->end->previous->next = list->end;
+	}
+	++list->size;
+}
+
+//Merges two lists into one
+void merge(List *list, List *left, List *right, const bool cmpByName)
+{
+	auto mergeNode = list->start;
+	auto leftNode = left->start;
+	auto rightNode = right->start;
+
+	while (leftNode != nullptr || rightNode != nullptr)
+	{
+		if (leftNode == nullptr || (rightNode != nullptr && cmpByName ? leftNode->name.compare(rightNode->name) : leftNode->number.compare(rightNode->number)))
+		{
+			mergeNode->name = rightNode->name;
+			mergeNode->number = rightNode->number;
+			rightNode = rightNode->next;
+		}
+		else
+		{
+			mergeNode->name = leftNode->name;
+			mergeNode->number = leftNode->number;
+			leftNode = leftNode->next;
+		}
+		mergeNode = mergeNode->next;
+	}
+}
+
+//Splits one list into two
+void split(List *list, List *left, List *right)
+{
+	auto temp = list->start;
+	for (int i = 1; i <= list->size; ++i)
+	{
+		if (i <= list->size / 2)
+		{
+			push(left, temp->name, temp->number);
+		}
+		else
+		{
+			push(right, temp->name, temp->number);
+		}
+		temp = temp->next;
+	}
+}
--- Homework-6/hw-6.4/list.h
+++ Homework-6/hw-6.4/list.h
@@ -1,1 +1,23 @@
 #pragma once
+
+struct Node;
+
+struct List;
+
+//Creates a new list
+List *createNewList();
+
+//Checks if the list is empty
+bool isEmpty(List *list);
+
+//Deletes the list
+void deleteList(List *list);
+
+//Creates a nww node at the end of the list
+void push(List *list, const string name, const string number);
+
+//Merges two lists into one
+void merge(List *list, List *left, List *right, const bool cmpByName);
+
+//Splits one list into two
+void split(List *list, List *left, List *right);

2bc3bed9d177e0156368e11b0154f1dac0888f11
Timur Zinnatulin <teemychteemych@gmail.com>
2018-12-09 14:23:18 +0300

    Finished main program module. Solution complete!

--- Homework-6/hw-6.4/hw-6.4.cpp
+++ Homework-6/hw-6.4/hw-6.4.cpp
@@ -10,7 +10,36 @@ int main()
 {
 	if (testingRoutine())
 	{
-		cout << "This is a template for merge sort program.\n";
+		cout << "Testing complete.\n";
 	}
+	cout << "Welcome to merge sort program ver.1!\n";
+	auto list = createNewList();
+	cout << "Opening mergesort.txt...\n";
+	openFile("mergesort.txt", list);
+	if (isEmpty(list))
+	{
+		cout << "The file is empty!\n";
+		deleteList(list);
+		return 0;
+	}
+	cout << "File successfully read.\n\n";
+	cout << "How do you want to sort the phonebook?\n";
+	cout << "Sort it by name? (Y/N): ";
+	char byName = '\0';
+	cin >> byName;
+	if (byName == 'Y')
+	{
+		cout << "Sorting by name...\n";
+	}
+	else
+	{
+		cout << "Sorting by number...\n";
+	}
+	bool cmpByName = (byName == 'Y' ? true : false);
+	mergeSort(list, cmpByName);
+	cout << "Phonebook sorted successfully. Printing...\n";
+	printList(list);
+	cout << "Exiting program...";
+	deleteList(list);
 	return 0;
 }

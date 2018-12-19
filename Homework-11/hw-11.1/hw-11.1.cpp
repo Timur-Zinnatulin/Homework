b13275d47e8de4aa616bb01066d19da42d1c8746
Timur Zinnatulin <teemychteemych@gmail.com>
2018-12-19 22:45:08 +0300

    Finished main program module. Solution complete!

--- Homework-11/hw-11.1/hw-11.1.cpp
+++ Homework-11/hw-11.1/hw-11.1.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <string>
+#include <fstream>
 #include "kmp-algo.h"
 #include "testing-routine.h"
 
@@ -15,5 +17,21 @@ int main()
 		cout << "Testing failed.\n";
 		return 1;
 	}
+	cout << "Enter the string you want to find in the text: \n";
+	string input;
+	getline(cin, input);
+	ifstream fin("input.txt");
+	cout << "Starting to look for it in input.txt...\n";
+	const int answer = substringPosition(fin, input);
+	if (answer == -1)
+	{
+		cout << "The string was not found in the text.\n";
+	}
+	else
+	{
+		cout << "The string was found in the text in position " << answer << ".\n";
+	}
+	fin.close();
+	cout << "\nProgram complete. Exiting...\n";
 	return 0;
 }

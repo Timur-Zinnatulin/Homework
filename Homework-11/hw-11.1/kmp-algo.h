31efff84fa4cb8252b3427996e7a3a1d1de4b8e7
Timur Zinnatulin <teemychteemych@gmail.com>
2018-12-19 22:29:46 +0300

    Added the algorithm module

--- Homework-11/hw-11.1/kmp-algo.h
+++ Homework-11/hw-11.1/kmp-algo.h
@@ -1,1 +1,7 @@
 #pragma once
+
+#include <fstream>
+#include <string>
+
+//Knuth-Morris-Pratt algorithm that returns the first position where substring is met
+int substringPosition(std::ifstream &fin, const std::string substring);

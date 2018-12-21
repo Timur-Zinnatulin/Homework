4a138c5a8afb92156635a152e3f7ce9c910801d2
Timur Zinnatulin <teemychteemych@gmail.com>
2018-12-21 14:40:13 +0300

    Temporary commit for test-3.3

--- Test-3/test-3.3/test-3.3.cpp
+++ Test-3/test-3.3/test-3.3.cpp
@@ -1,44 +1,31 @@
 ﻿#include <iostream>
 #include <vector>
-#include <algorithm>
+#include <fstream>
 
 using namespace std;
+
+bool isAccessible(const int vertice)
+{
+	return false;
+}
 
 int main()
 {
 	int vertices = 0;
-	int paths = 0;
-	cout << "Enter the amount of vertices and amount of paths.\n";
-	cin >> vertices >> paths;
-	vector<vector<int> > graph(vertices, vector<int>(vertices));
-	for (int i = 0; i < paths; ++i)
+	int edges = 0;
+	std::ifstream fin("input.txt");
+	fin >> vertices >> edges;
+	vector<vector<int>> graph(vertices, vector<int>(edges));
+	for (int i = 0; i < vertices; ++i)
 	{
-		int a = 0, b = 0;
-		cin >> a >> b;
-		graph[a - 1][b - 1] = 1;
-		graph[b - 1][a - 1] = -1;
-	}
-	for (int k = 0; k < vertices; ++k)
-	{
-		for (int i = 0; i < vertices; ++i)
+		for (int j = 0; j < edges; ++j)
 		{
-			for (int j = 0; j < vertices; ++k)
-			{
-				graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
-			}
+			fin >> graph[i][j];
 		}
 	}
 	for (int i = 0; i < vertices; ++i)
 	{
-		bool ifAccessible = false;
-		for (int j = 0; i < vertices; ++j)
-		{
-			if (i != j && graph[i][j] == 0)
-			{
-				ifAccessible = false;
-			}
-		}
-		if (ifAccessible)
+		if (isAccessible(i))
 		{
 			cout << "Vertice " << i + 1 << " is accessible.\n";
 		}

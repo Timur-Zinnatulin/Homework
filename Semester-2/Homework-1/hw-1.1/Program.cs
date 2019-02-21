0a122fdd17aa01a672fd42540a9e866e17501c07
Timur Zinnatulin <teemychteemych@gmail.com>
2019-02-21 21:02:19 +0300

    Completed problem 1.1

---
+++ Semester-2/Homework-1/hw-1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace hw_1._1
+{
+    class Program
+    {
+        private static int Factorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Factorial of 4 is {Factorial(4)}.");
+        }
+    }
+}

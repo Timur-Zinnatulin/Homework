c391c20bcc5936981f1cc9287ea6afae5f7a6086
Timur Zinnatulin <teemychteemych@gmail.com>
2019-02-21 21:04:12 +0300

    Completed problem 1.2

---
+++ Semester-2/Homework-1/hw-1.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace hw_1._2
+{
+    class Program
+    {
+        private static int Fibonacci(int n)
+        {
+            if ((n == 1) || (n == 2))
+            {
+                return 1;
+            }
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an index of a Fibonacci number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The Fibonacci number is {Fibonacci(n)}");
+        }
+    }
+}

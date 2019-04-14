e9ca89b2a32cee2a688f30e9c60533aa73d7625c
Timur Zinnatulin <teemychteemych@gmail.com>
2019-04-14 13:03:33 +0300

    Initial commit for StackCalculator

---
+++ Semester-2/Homework-2/StackCalculator/StackCalculator.Test/StackCalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿namespace StackCalculator.Test
+{
+    using System;
+    using StackCalculatorNamespace;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class StackCalculatorTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+    }
+}

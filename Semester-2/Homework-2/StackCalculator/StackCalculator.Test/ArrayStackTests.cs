ebeb0002629749c91108a7bbca0cb67f4799d1bd
Timur Zinnatulin <teemychteemych@gmail.com>
2019-04-22 18:19:58 +0300

    Added array stack tests.

---
+++ Semester-2/Homework-2/StackCalculator/StackCalculator.Test/ArrayStackTests.cs
@@ -0,0 +1,74 @@
+﻿namespace StackCalculator.Test
+{
+    using System;
+    using StackCalculatorNamespace;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ArrayStackTests
+    {
+        ArrayStack stack;
+
+        [SetUp]
+        public void SetUp()
+        {
+            stack = new ArrayStack(100);
+        }
+
+        [Test]
+        public void EmptyStackTest()
+        {
+            Assert.IsTrue(stack.IsEmpty());
+        }
+
+        [Test]
+        public void StackChangesSizeOnPushTest()
+        {
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Size);
+        }
+
+        [Test]
+        public void StackElementValueIsCorrectTest()
+        {
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Peek());
+        }
+
+        [Test]
+        public void PushesMoreThatONeElementTest()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Size);
+        }
+
+        [Test]
+        public void PopsOutOfStackTest()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Pop());
+            Assert.AreEqual(1, stack.Size);
+        }
+
+        [Test]
+        public void PopsLastElementFromStackTest()
+        {
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Pop());
+            Assert.IsTrue(stack.IsEmpty());
+        }
+
+        [Test]
+        public void PopExceptionTest()
+        {
+            Assert.Throws<InvalidOperationException>(_EmptyStackPop, "Stack is empty!");
+        }
+
+        private void _EmptyStackPop()
+        {
+            stack.Pop();
+        }
+    }
+}

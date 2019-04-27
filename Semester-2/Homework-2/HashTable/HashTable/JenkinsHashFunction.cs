b6edd26970e71d21f4f049a93f59297264769530
Timur Zinnatulin <teemychteemych@gmail.com>
2019-04-27 12:07:08 +0300

    Added Jenkins hash function

---
+++ Semester-2/Homework-2/HashTable/HashTable/JenkinsHashFunction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HashTable
+{
+    /// <summary>
+    /// Jenkins Hash Function class
+    /// </summary>
+    public class JenkinsHashFunction
+    {
+        /// <summary>
+        /// Jenkins hash function calculator
+        /// </summary>
+        /// <param name="word">Encoded word</param>
+        /// <returns>Hash function of the word</returns>
+        public int Key(string word)
+        {
+            int hash = 0;
+            for (int i = 0; i < word.Length; ++i)
+            {
+                hash += Convert.ToInt32(word[i]);
+                hash += hash << 10;
+                hash ^= hash >> 6;
+            }
+
+            hash += hash << 3;
+            hash ^= hash >> 11;
+            hash += hash << 15;
+
+            return hash;
+        }
+    }
+}

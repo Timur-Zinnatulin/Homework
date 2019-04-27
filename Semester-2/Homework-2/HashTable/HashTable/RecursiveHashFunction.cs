a636bbbb3a390ebf3ffc2d5998deee76788d18ce
Timur Zinnatulin <teemychteemych@gmail.com>
2019-04-27 16:00:21 +0300

    Added recursive hash function and fixed some little problems.

---
+++ Semester-2/Homework-2/HashTable/HashTable/RecursiveHashFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HashTable
+{
+    /// <summary>
+    /// Recursive hash function class
+    /// </summary>
+    public class RecursiveHashFunction
+    {
+        /// <summary>
+        /// Recursive hash function calculator
+        /// </summary>
+        /// <param name="word">Encoded word</param>
+        /// <returns>Hash function of the word</returns>
+        public ulong Key(string word)
+        {
+            ulong hash = 0;
+            ulong primeBase = 1;
+
+            foreach (ulong character in word)
+            {
+                hash += character * primeBase;
+                primeBase *= 129;
+            }
+
+            return hash;
+        }
+    }
+}

fef8f89aaa4b38c53c34d09c56750d953dac89c2
Timur Zinnatulin <teemychteemych@gmail.com>
2019-05-09 14:09:36 +0300

    Added operation subclass of a node and created addition subclass that I will template onto all other operations.

---
+++ Semester-2/Homework-4/ParseTree/ParseTree/Addition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ParseTree
+{
+    /// <summary>
+    /// Operation subclass that represents addition
+    /// </summary>
+    public class Addition : Operation
+    {
+        /// <summary>
+        /// Calculates the node value
+        /// </summary>
+        /// <returns>Value of node after calculation</returns>
+        public override int Calculate()
+            => LeftChild.Calculate() + RightChild.Calculate();
+
+        /// <summary>
+        /// Prints node's value
+        /// </summary>
+        public override void Print()
+        {
+            Console.Write("( + ");
+            LeftChild.Print();
+            RightChild.Print();
+            Console.Write(") ");
+        }
+    }
+}
---
+++ Semester-2/Homework-4/ParseTree/ParseTree/Operation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ParseTree
+{
+    /// <summary>
+    /// Node subclass that represents operation
+    /// </summary>
+    public abstract class Operation : Node
+    {
+        /// <summary>
+        /// Left child of a node
+        /// </summary>
+        public Node LeftChild { get; set; }
+
+        /// <summary>
+        /// Right child of a node
+        /// </summary>
+        public Node RightChild { get; set; }
+    }
+}

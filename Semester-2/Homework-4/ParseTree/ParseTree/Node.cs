e12f481dfe174d37e60b1595c8d755cfa594f0a9
Timur Zinnatulin <teemychteemych@gmail.com>
2019-05-09 13:52:27 +0300

    Added abstract node class.

---
+++ Semester-2/Homework-4/ParseTree/ParseTree/Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ParseTree
+{
+    /// <summary>
+    /// Parse tree node class
+    /// </summary>
+    public abstract class Node
+    {
+        /// <summary>
+        /// Left child of a node
+        /// </summary>
+        public Node LeftChild { get; set; }
+
+        /// <summary>
+        /// Right child of a node
+        /// </summary>
+        public Node RightChild { get; set; }
+
+        /// <summary>
+        /// Value of node after calculation
+        /// </summary>
+        public abstract int Calculate { get; }
+
+        /// <summary>
+        /// Prints node's value
+        /// </summary>
+        public abstract void Print();
+    }
+}

4f951ef190626e226116aa8feeb2f9d43512173d
Timur Zinnatulin <teemychteemych@gmail.com>
2019-05-10 11:16:26 +0300

    Created Expression Tree creator class and added XML documentation where it was needed. Ready to create unit tests.

---
+++ Semester-2/Homework-4/ParseTree/ParseTree/ExpressionCalculator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace ParseTree
+{
+    /// <summary>
+    /// Expression tree creation class
+    /// </summary>
+    public static class ExpressionCalculator
+    {
+        /// <summary>
+        /// Creates a new node for expression tree
+        /// </summary>
+        /// <param name="inputs">Queue of key symbols and numbers</param>
+        /// <returns>Created expression tree node</returns>
+        private static Node CreateNode(Queue<string> inputs)
+        {
+            var nextMatch = inputs.Peek();
+
+            if (nextMatch == "(")
+            {
+                Operation newNode;
+                inputs.Dequeue();
+                nextMatch = inputs.Peek();
+                switch(nextMatch)
+                {
+                    case "+":
+                        {
+                            newNode = new Addition();
+                            break;
+                        }
+                    case "-":
+                        {
+                            newNode = new Subtraction();
+                            break;
+                        }
+                    case "*":
+                        {
+                            newNode = new Multiplication();
+                            break;
+                        }
+                    case "/":
+                        {
+                            newNode = new Division();
+                            break;
+                        }
+                    default:
+                        {
+                            throw new ArgumentException();
+                        }
+                }
+                inputs.Dequeue();
+                newNode.LeftChild = CreateNode(inputs);
+                newNode.RightChild = CreateNode(inputs);
+                return newNode;
+            }
+            else
+            {
+                Number newNode = new Number(int.Parse(nextMatch));
+                inputs.Dequeue();
+                return newNode;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new expression tree with given input
+        /// </summary>
+        /// <param name="input">Unedited expression</param>
+        /// <returns>Exprettion tree for given expression</returns>
+        public static Tree CreateTree(string input)
+        {
+            Tree newTree;
+            const string options = @"-*\d+|[+-*/(]";
+            var matches = Regex.Matches(input, options);
+            var queue = new Queue<string>();
+
+            foreach (Match match in matches)
+            {
+                queue.Enqueue(match.Value);
+            }
+
+            var head = CreateNode(queue);
+
+            newTree = new Tree(head);
+
+            if (queue.Count > 0)
+            {
+                throw new ArgumentException();
+            }
+
+            return newTree;
+        }
+    }
+}
--- Semester-2/Homework-4/ParseTree/ParseTree/Tree.cs
+++ Semester-2/Homework-4/ParseTree/ParseTree/Tree.cs
@@ -9,17 +9,33 @@ namespace ParseTree
     {
         private Node root;
 
+        /// <summary>
+        /// Parse tree constructor
+        /// </summary>
+        /// <param name="rootNode">Root node of the tree</param>
         public Tree(Node rootNode)
         {
             this.root = rootNode;
         }
 
+        /// <summary>
+        /// Empty tree flag
+        /// </summary>
+        /// <returns>True if the tree is empty, false otherwise</returns>
         public bool IsEmpty()
             => root == null;
 
+        /// <summary>
+        /// Calculates the parsed expression
+        /// </summary>
+        /// <returns>Result of calculation</returns>
         public int Calculate()
             => root.Calculate();
 
+        /// <summary>
+        /// Puts parsed expression into prefix notation
+        /// </summary>
+        /// <returns>Parsed expression string</returns>
         public string Print()
             => root.Print();
     }

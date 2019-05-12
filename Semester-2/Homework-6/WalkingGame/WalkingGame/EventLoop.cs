741b06d81ade7b72afd25fa2c6fe783f8eb0bc9e
Timur Zinnatulin <teemychteemych@gmail.com>
2019-05-12 22:19:26 +0300

    Initial commit for problem 6.2 with EventLoop from the lecture.

---
+++ Semester-2/Homework-6/WalkingGame/WalkingGame/EventLoop.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace WalkingGame
+{
+    /// <summary>
+    /// Event loop class
+    /// </summary>
+    public class EventLoop
+    {
+        public event EventHandler<EventArgs> LeftHandler = (sender, args) => { };
+        public event EventHandler<EventArgs> RightHandler = (sender, args) => { };
+        public event EventHandler<EventArgs> UpHandler = (sender, args) => { };
+        public event EventHandler<EventArgs> DownHandler = (sender, args) => { };
+
+        /// <summary>
+        /// Perpetual program run loop
+        /// </summary>
+        public void Run()
+        {
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        {
+                            LeftHandler(this, EventArgs.Empty);
+                            break;
+                        }
+                    case ConsoleKey.RightArrow:
+                        {
+                            RightHandler(this, EventArgs.Empty);
+                            break;
+                        }
+                    case ConsoleKey.UpArrow:
+                        {
+                            UpHandler(this, EventArgs.Empty);
+                            break;
+                        }
+                    case ConsoleKey.DownArrow:
+                        {
+                            DownHandler(this, EventArgs.Empty);
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}

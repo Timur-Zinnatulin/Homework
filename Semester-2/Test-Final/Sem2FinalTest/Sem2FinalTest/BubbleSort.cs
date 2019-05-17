e378c7d268ec68d4f7ac1b740bce138b2e2b14dd
Timur Zinnatulin <teemychteemych@gmail.com>
2019-05-17 15:01:05 +0300

    Created solution for test problem 2.1. It should be correct.

---
+++ Semester-2/Test-Final/Sem2FinalTest/Sem2FinalTest/BubbleSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sem2FinalTest
+{
+    /// <summary>
+    /// Bubble sort class
+    /// </summary>
+    /// <typeparam name="T">Type of sorted list</typeparam>
+    public static class BubbleSort<T>
+    {
+        /// <summary>
+        /// Sort method 
+        /// </summary>
+        /// <param name="list">List that we need to sort</param>
+        /// <param name="comp">Comparer</param>
+        /// <param name="flagAscending"></param>
+        /// <returns>Sorted list</returns>
+        public static List<T> Sort(List<T> list, Comparer<T> comp)
+        {
+            bool ifSorted = true;
+
+            do
+            {
+                ifSorted = true;
+                for (int i = 0; i < (list.Count - 1); ++i)
+                {
+                    if (!comp.Compare(list[i], list[i + 1]))
+                    {
+                        ifSorted = false;
+                        var temp = list[i];
+                        list[i] = list[i + 1];
+                        list[i + 1] = temp;
+                    }
+                }
+            } while (!ifSorted);
+
+            return list;
+        }
+    }
+}
---
+++ Semester-2/Test-Final/Sem2FinalTest/Sem2FinalTest/DescendingComp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sem2FinalTest
+{
+    /// <summary>
+    /// Descending order comparison class
+    /// </summary>
+    /// <typeparam name="T">Type of compared element</typeparam>
+    public class DescendingComp<T> : Comparer<T>
+    {
+        /// <summary>
+        /// Compare method for comparer
+        /// </summary>
+        /// <param name="left">Left object</param>
+        /// <param name="right">Right object</param>
+        /// <returns>Result of comparison</returns>
+        public override bool Compare(T left, T right)
+        {
+            if (left.GetHashCode() >= right.GetHashCode())
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}

4827b9aa8b6a86a1131f531592db43394e8062be
Timur <teemychteemych@gmail.com>
2019-09-26 19:53:48 +0300

    Wrote ThreadPool constructor.

--- Semester-3/Homework-3/MyThreadPool/ThreadPool.cs
+++ Semester-3/Homework-3/MyThreadPool/ThreadPool.cs
@@ -1,8 +1,25 @@
 ﻿using System;
+using System.Threading;
 
 namespace ThreadPool
 {
+    /// <summary>
+    /// Self-made ThreadPool class
+    /// </summary>
     public class ThreadPool
     {
+        private Thread[] threads;
+
+        private bool[] isOccupied;
+
+        public ThreadPool(int n)
+        {
+            threads = new Thread[n];
+            isOccupied = new bool[n];
+            for (int i = 0; i < n; ++i)
+            {
+                isOccupied[i] = false;
+            }            
+        }
     }
 }

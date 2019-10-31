3ed87d4737860d28ab399be2ce42d28e39615e7c
Timur <teemychteemych@gmail.com>
2019-10-31 23:29:21 +0300

    Initial commit (setting up files and references).

---
+++ Semester-3/Test-1/Test-1-Tests/TaskTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using Test_1;
+
+namespace Test_1_Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

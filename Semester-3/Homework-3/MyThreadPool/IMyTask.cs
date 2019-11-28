b942c037db76d8135f01edb877f0567ae9eeeeaa
Timur <teemychteemych@gmail.com>
2019-11-28 20:04:40 +0300

    Added comments to IMyTask

--- Semester-3/Homework-3/MyThreadPool/IMyTask.cs
+++ Semester-3/Homework-3/MyThreadPool/IMyTask.cs
@@ -9,10 +9,23 @@ namespace ThreadPool
     /// </summary>
     public interface IMyTask<TResult>
     {
+        /// <summary>
+        /// Returns true if the task is completed, false if not.
+        /// </summary>
         bool IsCompleted { get; private set; }
 
+        /// <summary>
+        /// Returns task result. BLocks called thread until the result is ready.
+        /// </summary>
+        /// <value></value>
         TResult Result { get; }
 
+        /// <summary>
+        /// Generates new task based on the current task.
+        /// </summary>
+        /// <param name="function">New task function</param>
+        /// <typeparam name="TNewResult">Type of new function result</typeparam>
+        /// <returns>Result of composotion of functions</returns>
         IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> function);
     }
 }

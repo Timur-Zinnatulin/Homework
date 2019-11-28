b76d56e5a3cd9f3ee1dd89fd57d01b6aed6d4f3a
Timur <teemychteemych@gmail.com>
2019-11-28 16:13:28 +0300

    I better start this task over...

--- Semester-3/Homework-3/MyThreadPool/MyTask.cs
+++ Semester-3/Homework-3/MyThreadPool/MyTask.cs
@@ -9,46 +9,5 @@ namespace ThreadPool
     /// </summary>
     public class MyTask<TResult> : IMyTask<TResult>
     {
-        public MyTask(Func<TResult> function)
-        {
-            this.function = function;
-            this.Result = null;
-            this.IsCompleted = false;
-        }
-        private object lockObject = new Object();
-        private Func<TResult> function;
-        private ManualResetEvent waitResult = new ManualResetEvent(false);
-
-        private Exception internalException = null;
-
-        private TResult calculationResult;
-
-        /// <summary>
-        /// Result of Task result calculation
-        /// </summary>
-        public TResult Result 
-        { 
-            get
-            {
-                waitResult.WaitOne();
-                if (internalException == null)
-                {
-                    return calculationResult;
-                }
-
-                throw new AggregateException(internalException);
-            } 
-        }
-
-        public void ExecuteTask(bool normalExecution = false)
-        {
-            
-        }
-        /// <summary>
-        /// Method that lets us calculate composition of functions in a single task
-        /// </summary>
-        /// <returns>Result of composition</returns>
-        public MyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> function)
-            => new MyTask<TNewResult>(() => function(this.Result));
     }
 }
--- Semester-3/Homework-3/MyThreadPool/ThreadPool.cs
+++ Semester-3/Homework-3/MyThreadPool/ThreadPool.cs
@@ -9,53 +9,5 @@ namespace ThreadPool
     /// </summary>
     public class ThreadPool
     {
-        private Thread[] threads;
-
-        private volatile bool[] isOccupied;
-
-        private ConcurrentQueue<Action<bool>> taskQueue;
-        private CancellationTokenSource source;
-        private CancellationToken token;
-
-        public ThreadPool(int n)
-        {
-            threads = new Thread[n];
-            isOccupied = new bool[n];
-            taskQueue = new ConcurrentQueue<MyTask>();
-            source = new CancellationTokenSource();
-            token = source.Token;
-            for (int i = 0; i < n; ++i)
-            {
-                var index = i;
-                isOccupied[i] = false;
-                threads[index] = new Thread(() => 
-                    {
-                        while (!token.IsCancellationRequested)
-                        {
-                            if (taskQueue.TryDequeue(out Action<bool> task))
-                            {
-                                isOccupied[index] = true;
-                                var answer = taskQueue.Dequeue().Result;
-                                isOccupied[index] = false;
-                            }
-                        }
-                    }
-                );
-                threads[index].Start();
-            }
-        }
-
-        public void Submit(MyTask task)
-        {
-            if (!token.IsCancellationRequested)
-            {
-                taskQueue.Enqueue(task);
-                return;
-            }
-            throw new AggregateException("Attempted to add task after shutdownn.");
-        }
-
-        public void Shutdown()
-            => source.Cancel;
     }
 }

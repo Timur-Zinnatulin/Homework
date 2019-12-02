fc2bfb8f7820c28870d936484e31c80f50b84703
Timur <teemychteemych@gmail.com>
2019-12-03 00:08:25 +0300

    Created test project

---
+++ Semester-3/Homework-3/ThreadPoolTests/ThreadPoolTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using ThreadPool;
+
+namespace ThreadPoolTests
+{
+    [TestFixture]
+    public class ThreadPoolTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

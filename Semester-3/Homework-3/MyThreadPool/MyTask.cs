90b4f62b719ecf6c79224babef5ef60b59e1a4df
Timur <teemychteemych@gmail.com>
2019-12-02 18:42:48 +0300

    Working on MyTask

--- Semester-3/Homework-3/MyThreadPool/MyTask.cs
+++ Semester-3/Homework-3/MyThreadPool/MyTask.cs
@@ -5,9 +5,49 @@ using System.Threading;
 namespace ThreadPool
 {
     /// <summary>
-    /// ThreadPool task
+    /// ThreadPool generated task
     /// </summary>
     public class MyTask<TResult> : IMyTask<TResult>
     {
+        /// <summary>
+        /// Task supplier function
+        /// </summary>
+        private readonly Func<TResult> supplier;
+
+        /// <summary>
+        /// MyThreadPool that created this task
+        /// </summary>
+        private MyThreadPool threadPool;
+
+        /// <summary>
+        /// Result of task calculation
+        /// </summary>
+        private TResult taskResult;
+
+        /// <summary>
+        /// Blocks the thread until it returns task result
+        /// </summary>
+        private ManualResetEvent threadBlocker = new ManualResetEvent(false);
+
+        public MyTask(MyThreadPool pool, Func<TResult> supplier)
+        {
+            this.threadPool = pool;
+            this.supplier = supplier;
+        }
+
+        public bool IsCompleted { get; private set; } = false;
+
+        public TResult Result
+        {
+            get
+            {
+                threadBlocker.WaitOne();
+
+                if (!IsCompleted)
+                {
+                    
+                }
+            }
+        }
     }
 }

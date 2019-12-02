029782f2cb10e41fd0417abe9d9b204e586c6077
Timur <teemychteemych@gmail.com>
2019-12-02 21:58:12 +0300

    Started work on MyThreadPool

--- Semester-3/Homework-3/MyThreadPool/ThreadPool.cs
+++ Semester-3/Homework-3/MyThreadPool/ThreadPool.cs
@@ -9,5 +9,32 @@ namespace ThreadPool
     /// </summary>
     public class ThreadPool
     {
+        /// <summary>
+        /// Amount of threads in pool
+        /// </summary>
+        private readonly Thread[] threads;
+
+        /// <summary>
+        /// Queue for tasks ready to be executed
+        /// </summary>
+        private ConcurrentQueue<Action> taskQueue = new ConcurrentQueue<Action>();
+
+        /// <summary>
+        /// Cancellation token
+        /// </summary>
+        /// <returns></returns>
+        private CancellationTokenSource cancel = new CancellationTokenSource();
+
+        /// <summary>
+        /// Unlocks availeble threads
+        /// </summary>
+        private readonly AutoResetEvent threadBlocker = new AutoResetEvent(false);
+
+        /// <summary>
+        /// Amount of available threads
+        /// </summary>
+        private volatile int availableThreads;
+
+        
     }
 }

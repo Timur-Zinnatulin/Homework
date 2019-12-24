c5374f63e72a318adf9dd473dd6f87271bfd3536
Timur <teemychteemych@gmail.com>
2019-12-24 15:38:07 +0300

    Added WriteDirPath function

--- Semester-3/Homework-5/FTP-Server/Server.cs
+++ Semester-3/Homework-5/FTP-Server/Server.cs
@@ -87,11 +87,152 @@ namespace Server
             listener.Stop();
         }
 
+        /// <summary>
+        /// Supplies the client with information on demand
+        /// </summary>
         private void SupplyClient(object clientObject)
         {
             var client = (TcpClient)clientObject;
 
-            
+            IPAddress clientIP;
+            try
+            {
+                clientIP = ((IPEndPoint)client.Client.RemoteEndPoint).Address;
+                var inputStream = new StreamReader(client.GetStream());
+                var outputStream = new StreamWriter(client.GetStream());
+
+                var commandType = new char[1];
+                await inputStream.ReadAsync(commandType, 0, 1);
+
+                if (commandType[0] == '1' || commandType[0] == '2')
+                {
+                    var path = await inputStream.ReadLineAsync();
+
+                    switch (commandType[0])
+                    {
+                        case '1':
+                        {
+                            Console.WriteLine($"Client {clientIP} requested directory contents.");
+                            this.WriteDirPath(path, outputStream);
+                        }
+
+                        case '2':
+                        {
+                            Console.WriteLine($"Client {clientIP} requested file.");
+                            this.WriteFileBytes(path, outputStream);
+                        }
+                    }
+                }
+            }
+
+            catch (System.IO.Exception)
+            {
+                this.HandleClientDisconnected();
+                --this.currentConnections;
+                return;
+            }
+
+            client.Close();
+            --this.currentConnections;
+
+            Console.WriteLine($"Client {clientIP} disconnected.");
         }
+        
+        /// <summary>
+        /// Writes about directory by given path
+        /// </summary>
+        private void WriteDirPath(string path, StreamWriter output)
+        {
+            var rootPath = Path.GetTempPath();
+            if (path != "~")
+            {
+                rootPath += path; 
+            }
+
+            // Get info about directory
+            DirectoryInfo directory;
+            try
+            {
+                directory = new DirectoryInfo(rootPath);
+            }
+
+            catch (SecurityException)
+            {
+
+            }
+            catch (ArgumentException)
+            {
+
+            }
+            catch (PathTooLongException)
+            {
+
+            }
+
+            if (!directory.Exists)
+            {
+                this.HandleIncorrectPath(output, "The path is too long.");
+            }
+
+            FileSystemInfo[] directoryContents;
+
+            // Get info about files in directory
+            try
+            {
+                directoryContents = directory.GetFileSystemInfos();
+            }
+
+            catch (UnauthorizedAccessException)
+            {
+
+            }
+
+            // Write length of output
+            try
+            {
+                output.Write($"{directoryContents.Length}");
+            }
+
+            catch (ObjectDisposedException)
+            {
+
+            }
+
+            foreach (var entity in directoryContents)
+            {
+                // Write about each entity (file | directory) in this directory
+                try
+                {
+                    if (entity is DirectoryInfo)
+                    {
+                        output.Write($"{((DirectotyInfo)entity).Name.Replace(' ', '/')} {true.ToString()}");
+                    }
+
+                    else if (entity is FileInfo)
+                    {
+                        output.Write($"{((FileInfo)entity).Name.Replace(' ', '/')} {false.ToString()}");
+                    }
+                }
+
+                catch (ObjectDisposedException)
+                {
+
+                }
+            }
+
+            // Finish the message
+            try
+            {
+                output.WriteLine();
+                output.Flush();
+            }
+
+            catch (ObjectDisposedException)
+            {
+
+            }
+        }
+
+        private void Write
     }
 }

77447c65fa84cc0029e44c3b1afb9cb1a22c8269
Timur <teemychteemych@gmail.com>
2019-12-25 13:51:38 +0300

    Finished writing testable projects!

--- Semester-3/Homework-8/MyTestProjects/Project2/Class1.cs
+++ Semester-3/Homework-8/MyTestProjects/Project2/Class1.cs
@@ -1,8 +1,24 @@
 ﻿using System;
 
+using MyNUnit.Annotations;
+
 namespace Project2
 {
     public class Class1
     {
+        [Before]
+        public static void Before1()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+        }
+
+        [After]
+        public static void After1()
+        {
+        }
     }
 }
--- Semester-3/Homework-8/MyTestProjects/Project5/Class1.cs
+++ Semester-3/Homework-8/MyTestProjects/Project5/Class1.cs
@@ -1,8 +1,24 @@
 ﻿using System;
 
+using MyNUnit.Annotations;
+
 namespace Project5
 {
     public class Class1
     {
+        [BeforeClass]
+        public static void Initialize()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+        }
+
+        [AfterClass]
+        public static void Clear()
+        {
+        }
     }
 }

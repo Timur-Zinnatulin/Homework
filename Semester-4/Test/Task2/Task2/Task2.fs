304c51cfe89f0423fee0f5233da71eb2c1cc95bf
Timur Zinnatulin <teemychteemych@gmail.com>
2020-05-03 17:42:30 +0300

    OMG I just noticed I didnt add this file in. It hasnt been modified since the test.

---
+++ Semester-4/Test/Task2/Task2/Task2.fs
@@ -0,0 +1,31 @@
+﻿module Task2
+
+open System
+
+let isProductOfTwoTripleDigits n =
+    let root = (double >> sqrt >> int) n
+    let rec findPairOfDivisors x =
+        if (x >= 100) then
+            if (n % x = 0) && (n / x >= 100) && (n / x < 1000) then
+                true
+            else
+                findPairOfDivisors (x - 1)
+        else
+            false
+    findPairOfDivisors root
+
+//Find the greates palindrome that is
+//a product of two triple-digit numbers
+let findMaxPalindrome () =
+    let rec findMaxPalindromeRec cur =
+        let currentNumber =
+            let strNumber = string cur
+            let rev = strNumber |> Seq.rev |> String.Concat
+            (strNumber + rev) |> int
+
+        if (isProductOfTwoTripleDigits currentNumber) then
+            currentNumber
+        else
+            findMaxPalindromeRec (cur - 1)
+
+    findMaxPalindromeRec 999

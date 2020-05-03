8528bfe59713f3bbb9024ebffbf81a3ac405c8e3
Timur Zinnatulin <teemychteemych@gmail.com>
2020-05-03 18:11:14 +0300

    Created first workflow.

---
+++ Semester-4/Homework-6/Workflow1/Workflow.fs
@@ -0,0 +1,12 @@
+﻿module Workflow1
+
+open System
+
+//Workflow for calculation with given accuracy
+type CalculationWorkflow(accuracy : int) =
+    let calcAccuracy = accuracy
+    member this.Bind(x : double, f : double -> double) =
+        f (Math.Round (x, accuracy))
+
+    member this.Return(x : double) =
+        Math.Round(x, accuracy)

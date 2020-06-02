09bfc42043086d7d9157ad8b02b0d8d4ffba9480
Timur Zinnatulin <teemychteemych@gmail.com>
2020-06-02 13:41:13 +0300

    Finished point free task. Some problems with testing.

---
+++ Semester-4/Homework-4/PointFree/PointFree.fs
@@ -0,0 +1,15 @@
+﻿module PointFree
+
+open System
+
+/// Original function
+let func x l = List.map (fun y -> y * x) l
+
+let func'1 x = List.map (fun y -> y * x)
+
+let func'2 x = List.map (fun y -> (*) x y)
+
+let func'3 x = List.map ((*) x)
+
+/// Point-free version
+let func'4 = List.map << (*)
